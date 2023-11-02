e315d0de28727ed873e0681ed845cdb8af8f0e89
AntonioDoberti <antonio.doberti@uc.cl>
2023-11-02 15:20:43 -0300

    jcnwj

--- spec/controllers/shopping_cart_controller_spec.rb
+++ spec/controllers/shopping_cart_controller_spec.rb
@@ -5,42 +5,85 @@ RSpec.describe ShoppingCartController, type: :controller do
   let(:product) { Product.create!(nombre: 'Test Product', categories: 'Cancha', stock: 10, precio: 100, user_id: user.id) }
 
   describe 'GET #show' do
-    it 'redirects to the root path when user is not logged in' do
+  context 'when user is logged in' do
+    before do
+      sign_in user
+    end
+
+    it 'renders the show template' do
       get :show
-      expect(response).to redirect_to(root_path)
-    end
-
-    context 'when user is logged in' do
-      before do
-        sign_in user
-      end
-
-      it 'renders the show template' do
-        get :show
-        expect(response).to render_template('show')
-      end
+      expect(response).to render_template('show')
     end
   end
 
-  describe 'GET #details' do
-    it 'redirects to the root path when user is not logged in' do
+  context 'when user is not logged in' do
+    it 'redirects to the root path' do
+      get :show
+      expect(response).to eq('')
+    end
+  end
+end
+
+describe "ShoppingCartsController#details" do
+  context "when the user is not logged in" do
+    it "redirects to the login page" do
+      # Simular que el usuario no está loggeado
+      session[:user_id] = nil
+
+      # Llamar al método `details`
       get :details
-      expect(response).to redirect_to(root_path)
-    end
 
-    context 'when user is logged in' do
-      before do
-        sign_in user
-      end
-
-      it 'renders the details template' do
-        get :details
-        expect(response).to render_template('details')
-      end
+      # Assert that the response is a redirect to the login page
+      expect(response).to redirect_to(login_path)
     end
   end
 
-  describe 'POST #insertar_producto' do
+  context "when the user is logged in and has a shopping cart" do
+    it "calculates the total price and renders the `details` template" do
+      # Simular que el usuario está loggeado y tiene un carrito de compras
+      user = create(:user)
+      sign_in user
+      shopping_cart = create(:shopping_cart, user: user)
+      @shopping_cart = shopping_cart
+
+      # Llamar al método `details`
+      get :details
+
+      # Assert that the response status is 200 OK
+      expect(response.status).to eq(200)
+
+      # Assert that the `details` template was rendered
+      expect(response).to render_template(:details)
+
+      # Assert that the total price was calculated correctly
+      expect(@total_pago).to eq(shopping_cart.precio_total + shopping_cart.costo_envio)
+    end
+  end
+
+  context "when the user is logged in but does not have a shopping cart" do
+    it "redirects to the shopping cart page with an alert message" do
+      # Simular que el usuario está loggeado pero no tiene un carrito de compras
+      user = create(:user)
+      sign_in user
+      @shopping_cart = nil
+
+      # Llamar al método `details`
+      get :details
+
+      # Assert that the response status is 302 Redirect
+      expect(response.status).to eq(302)
+
+      # Assert that the response redirects to the shopping cart page
+      expect(response).to redirect_to(carro_path)
+
+      # Assert that the alert message is set
+      expect(flash[:alert]).to eq('No tienes productos que comprar.')
+    end
+  end
+end
+
+describe 'POST #insertar_producto' do
+  context 'when user is logged in' do
     before do
       sign_in user
     end
@@ -51,6 +94,14 @@ RSpec.describe ShoppingCartController, type: :controller do
       expect(flash[:notice]).to eq('Producto agregado al carro de compras')
     end
   end
+
+  context 'when user is not logged in' do
+    it 'redirects to the root path' do
+      post :insertar_producto, params: { product_id: product.id, add: { amount: 2 } }
+      expect(response).to redirect_to(root_path)
+    end
+  end
+end
 
   describe 'DELETE #eliminar_producto' do
     before do
